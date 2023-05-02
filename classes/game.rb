fcc5a322ee3394e7111cbaf23b2f848932b5fd7f
Kevin Cho <kevcho604@gmail.com>
2023-05-01 20:26:07 -0700

    Add game start, end, turn switch, winner condition to game loop

--- classes/game.rb
+++ classes/game.rb
@@ -7,20 +7,47 @@ class Game
     @current_player = @player_1 
   end
 
-  def game_start
+
+  def start_game
     puts "Let's play a game >:)"
   end
 
 
+  def display_score
+    puts "Score: P1 has #{player_1.lives}/3 lives | P2 has #{player_2.lives}/3 lives"
+  end
+  
 
+  def switch_players
+    if @current_player == @player_1
+      @current_player = @player_2
+    else 
+      @current_player = @player_1
+    end
+  end
+
+
+  def winner 
+    if @player_1.lives == 0
+      @winner = @player_2
+    else 
+      @winner = @player_1
+    end
+  end 
+
+
+  def end_game
+    puts "-- Game Over --"
+    puts "#{@winner.name} wins with a score of #{@winner.lives}/3 lives!"
+    puts "-- Goodbye --"
 
   def play
-    game_start
+    start_game
 
-    while @player_1.lives > 0 || @player_2.live > 0 do
+    while @player_1.lives > 0 || @player_2.lives > 0 do
       puts "-- New Turn --"
       question = Question.new
-      question.generate_question(@current_player)
+      question.generate_question(@current_player.name)
       print "> "
       input = gets.chomp.to_i 
 
@@ -30,6 +57,11 @@ class Game
         puts "#{@current_player.name}: ...That is incorrect. Lose a life!"
         @current_player.lose_life
       end
+      display_score
+      switch_players
     end
+    winner
+    end_game
   end 
+  
 end 
